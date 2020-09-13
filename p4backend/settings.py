6422c999a61483d6b67844dfc809e5af48e5d772
Besrat Zerehaimanot <bzerehaimanot@gmail.com>
2020-09-12 21:55:45 -0400

    setting up authentication is half way complete

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', # for our rest api
+    'apps.authentication',
+    'apps.api',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,20 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+AUTH_USER_MODEL = 'authentication.User'
+
 
 
 # Internationalization
