e4b8951d5322c0a161a1f56e3856609d6996243f
Besrat Zerehaimanot <bzerehaimanot@gmail.com>
2020-09-18 03:20:01 -0400

    minor settings update

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,5 +4,5 @@ from .views import RegistrationAPIView, LoginAPIView, UserListViewSet
 urlpatterns = [
     url(r'^users/$', UserListViewSet.as_view({'get': 'list'}), name='user_list'),
     url(r'^users/register/$', RegistrationAPIView.as_view(), name='register'),
-    url(r'^users/login/$',LoginAPIView.as_view(), name='login'),
+    url(r'^users/login/$', LoginAPIView.as_view(), name='login'),
 ]
